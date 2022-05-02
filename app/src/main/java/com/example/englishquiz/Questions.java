a331ce4770dbb58d27f6ac55c23d085c2f3f3dbd
yudin5 <yudin5@yandex.ru>
2022-05-03 01:36:10 +0300

    add questions, remake design to have long answers

--- app/src/main/java/com/example/englishquiz/Questions.java
+++ app/src/main/java/com/example/englishquiz/Questions.java
@@ -16,7 +16,10 @@ public class Questions {
             new Question(q_9, q_9_op_1, q_9_op_2, q_9_explanation, q_9_answer),
             new Question(q_10, q_10_op_1, q_10_op_2, q_10_explanation, q_10_answer),
             new Question(q_11, q_11_op_1, q_11_op_2, q_11_explanation, q_11_answer),
-            new Question(q_12, q_12_op_1, q_12_op_2, q_12_explanation, q_12_answer)
+            new Question(q_12, q_12_op_1, q_12_op_2, q_12_explanation, q_12_answer),
+            new Question(q_13, q_13_op_1, q_13_op_2, q_13_explanation, q_13_answer),
+            new Question(q_14, q_14_op_1, q_14_op_2, q_14_explanation, q_14_answer),
+            new Question(q_15, q_15_op_1, q_15_op_2, q_15_explanation, q_15_answer)
     };
 
     public static Question[] getQuestionBank() {
